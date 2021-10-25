6f7bf927a2e2f5385f361449547799f198fc4118
vikas yadav <yadvikas610@gmail.com>
2021-10-25 20:51:36 +0530

    added sign up section and authentication with firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./Components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createrUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +18,20 @@ class App extends React.Component {
   unsubscribefromAuth = null;
 
   componentDidMount() {
-    this.unsubscribefromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribefromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createrUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else this.setState({ currentUser: userAuth });
     });
   }
 
